80a274455b8ec095bf6e3a98e38f5fe2b077f107
raga <ragamuthahari@gmail.com>
2019-02-01 09:40:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/majorc0der/splashscreenanimasi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.majorc0der.splashscreenanimasi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        ImageView logo = findViewById(R.id.logo_launcher);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                startActivity(new Intent(MainActivity.this, menu.class));
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        logo.startAnimation(animation);
+    }
+}
